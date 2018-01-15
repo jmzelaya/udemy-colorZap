bfe1d7b8e82ce709f9314be4c509a9f5ec8643ab
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-15 13:20:34 -0500

    added function to detect phone orientation and an image to prompt user to play in portrait mode

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -1,6 +1,11 @@
 var StateTitle = {
   preload: function (){
+    //Game logo
     game.load.image("logo", "images/title/gameLogo.png");
+    //Lock screen orientation for mobile
+    //Has 2 parameters (lanscape, portrait)
+    //Takes a boolean for each
+    game.scale.forceOrientation(false, true);
   },
 
   create:function (){
@@ -9,6 +14,29 @@ var StateTitle = {
     //Change the "anchor" point of the logo
     //to be the center of the logo (Between 0 and 1);
     this.logo.anchor.set(0.5, 0.5);
+    //Call the setListeners function
+    this.setListeners();
+  },
+
+  //Define ALL listeners in a separate function
+  //Allow for better organization
+  setListeners: function(){
+    game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
+    game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+  },
+
+  wrongWay: function(){
+    //Check to see if it detects the enterIncorrectOrientation
+    // console.log("wrongWay");
+    //When in wrongWay make the div #wrongWay display BLOCK
+    document.getElementById("wrongWay").style.display="block";
+  },
+
+  rightWay:function(){
+    //Check to see if it detects the leaveIncorrectOrientation
+    // console.log("rightWay");
+    //When in the rightWay make the div #wrongWay display NONE
+    document.getElementById("wrongWay").style.display="none";
   },
 
   update: function (){
