48c956fc2f1bc94095f4bf1160e912707dd955fc
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-15 12:37:43 -0500

    added viewport meta tag for mobile, and css to remove padding and margin to the body

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -4,7 +4,11 @@ var StateTitle = {
   },
 
   create:function (){
-    this.logo = game.add.sprite(0,0, "logo");
+    //Add the logo and center it
+    this.logo = game.add.sprite(game.world.centerX,180, "logo");
+    //Change the "anchor" point of the logo
+    //to be the center of the logo (Between 0 and 1);
+    this.logo.anchor.set(0.5, 0.5);
   },
 
   update: function (){
