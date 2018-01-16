50f0298217a5122b7878de52eef696ff6ad430c5
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-01-15 20:11:44 -0500

    added button sprite sheet, defined the start button and created startGame function, which load the MainState

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -1,11 +1,16 @@
 var StateTitle = {
   preload: function (){
-    //Game logo
+    //Load the game logo
     game.load.image("logo", "images/title/gameLogo.png");
+    //Load a spritesheet --> params (4, optional 5)
+    //refName, imgPath, imgWidth, imgHeight, optional: # of sprites
+    game.load.spritesheet("buttons", "images/ui/buttons.png", 265, 75);
     //Lock screen orientation for mobile
     //Has 2 parameters (lanscape, portrait)
     //Takes a boolean for each
-    game.scale.forceOrientation(false, true);
+    if (screen.width<900){
+      game.scale.forceOrientation(false, true);
+    }
   },
 
   create:function (){
@@ -14,15 +19,33 @@ var StateTitle = {
     //Change the "anchor" point of the logo
     //to be the center of the logo (Between 0 and 1);
     this.logo.anchor.set(0.5, 0.5);
+
+    //START BUTTON
+    //Params: x,y, imageKey (spritesheet refName), clickFunction, this(the scope),
+    //over_frame(frame#), normal_frame(frame#), down_frame(frame#)
+    this.btnStart = game.add.button(game.world.centerX , game.world.height-150,
+      "buttons", this.startGame, this, 7, 6, 7);
+    this.btnStart.anchor.set(0.5, 0.5);
+
     //Call the setListeners function
     this.setListeners();
+  },
+
+  startGame:function(){
+    //if we don't pass "this", then the "this" keyword
+    //will refer to the button and NOT the state
+    //Check to see if the button works with
+    // console.log("Start Game!");
+    game.state.start("StateMain");
   },
 
   //Define ALL listeners in a separate function
   //Allow for better organization
   setListeners: function(){
-    game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
-    game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+    if (screen.width<900){
+      game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
+      game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+    }
   },
 
   wrongWay: function(){
